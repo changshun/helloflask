9c26a919ff4aab38b7dda16e3cf233adede3a7bd
Changshun <chosen.shih@gmail.com>
2021-03-07 16:45:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
+    return '<h1>Hello Totoroo!</h1><img src="http://helloflask.com/totoro.gif">'
